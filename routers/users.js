b5f95232d8bc2b1f8402f57bb877ca42a1635f21
Mohammad Al osta <mohammad.alosta@sky.uk>
2023-11-10 10:41:20 +0000

    Define Routes (Users) done

--- routers/users.js
+++ routers/users.js
@@ -7,4 +7,20 @@ usersRouter.get("/", async (req, res) => {
     res.json(await User.findAll());
 })
 
+usersRouter.get("/:id", async (req, res) => {
+    res.json(await User.findByPk(req.params.id));
+})
+
+usersRouter.get("/shows/:id", async (req, res) => {
+    let watchedShows = await User.findByPk(req.params.id, {include: Show})
+    watchedShows = watchedShows.shows
+    res.json(watchedShows);
+})
+
+usersRouter.put("/", async (req, res) => {
+    await User.create(req.body);
+    res.json("Done.")
+})
+
+
 module.exports = usersRouter;
